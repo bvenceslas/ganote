1ab0e2bcd3b096d17fd64917f2a285764b637655
bvenceslas <bvenceslas@gmail.com>
2020-11-07 11:27:48 +0200

    Connect Backend to the MongoDb

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+require('./db/db');
 const express = require('express');
+
 //use environment variables
 require('dotenv').config();
 
 const app = express();
 app.use(express.json()); // replacing body-parser library
-
-
 
 
 const port = process.env.PORT || 5070;
