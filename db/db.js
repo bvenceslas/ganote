170c970a37970b01f59c5e06cef55bc4b449b791
bvenceslas <bvenceslas@gmail.com>
2020-11-09 12:05:38 +0200

    Add and Test All The Endpoints

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.DB_PATH, {useUnifiedTopology: true, useNewUrlParser: true}, () => {
+mongoose.connect(process.env.DB_PATH, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false}, () => {
     console.log('GaNote mongoDB connected successfuly');
 });
+mongoose.Promise = global.Promise;
