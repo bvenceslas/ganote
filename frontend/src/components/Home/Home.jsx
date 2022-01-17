6b564e3d45d3ebd52b558a007472399aa604ca44
bvenceslas <bvenceslas@gmail.com>
2022-01-17 09:11:32 +0200

    styled NoteList component

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,6 +1,12 @@
+import './index.css';
+import NoteList from "../NoteList/NoteList";
+
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="homepage">
+            Home here
+            <NoteList />
+        </div>
     );
 }
  
---
+++ frontend/src/components/NoteList/NoteList.jsx
@@ -0,0 +1,80 @@
+import './index.css';
+
+const NoteList = () => {
+    return ( 
+        <div className="note-list">
+            <h1>Vos notes</h1>
+
+            <div className="note-container">
+            <div className="note-wrapper">
+                    <h2 className="note-subject">How to get involved in Love</h2>
+                    <p className="note-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p className="note-decision">-- Urgent --</p>
+                    <div className="read-more">Read more</div>
+
+                </div>
+
+                <div className="note-wrapper">
+                    <h2 className="note-subject">How to get involved in Love</h2>
+                    <p className="note-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p className="note-decision">-- Urgent --</p>
+                    <div className="read-more">Read more</div>
+
+                </div>
+
+                <div className="note-wrapper">
+                    <h2 className="note-subject">How to get involved in Love</h2>
+                    <p className="note-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p className="note-decision">-- Urgent --</p>
+                    <div className="read-more">Read more</div>
+
+                </div>
+
+                <div className="note-wrapper">
+                    <h2 className="note-subject">How to get involved in Love</h2>
+                    <p className="note-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p className="note-decision">-- Urgent --</p>
+                    <div className="read-more">Read more</div>
+
+                </div>
+
+
+
+
+                <div className="note-wrapper">
+                    <h2 className="note-subject">How to get involved in Love</h2>
+                    <p className="note-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p className="note-decision">-- Urgent --</p>
+                    <div className="read-more">Read more</div>
+
+                </div>
+
+                <div className="note-wrapper">
+                    <h2 className="note-subject">How to get involved in Love</h2>
+                    <p className="note-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p className="note-decision">-- Urgent --</p>
+                    <div className="read-more">Read more</div>
+
+                </div>
+
+                <div className="note-wrapper">
+                    <h2 className="note-subject">How to get involved in Love</h2>
+                    <p className="note-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p className="note-decision">-- Urgent --</p>
+                    <div className="read-more">Read more</div>
+
+                </div>
+
+                <div className="note-wrapper">
+                    <h2 className="note-subject">How to get involved in Love</h2>
+                    <p className="note-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p className="note-decision">-- Urgent --</p>
+                    <div className="read-more">Read more</div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default NoteList;
