d2b00216082fb4b533d31a0cccc8e1b1db99b790
bvenceslas <bvenceslas@gmail.com>
2022-01-17 11:22:32 +0200

    styled note details

--- frontend/src/components/NoteDetails/NoteDetails.jsx
+++ frontend/src/components/NoteDetails/NoteDetails.jsx
@@ -1,6 +1,19 @@
+import './index.css';
+
 const NoteDetails = () => {
     return ( 
-        <h1>Note Details</h1>
+        <div className="note-details">
+            <h1>Details of {'NoteX'}</h1>
+
+            <div className="notedet-container">
+                <div className="notedet-wrapper">
+                    <h2 className="notedet-subject">How to get involved in Love</h2>
+                    <p className="notedet-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <p className="notedet-decision">-- Urgent --</p>
+                    <div className="delete">Delete Note</div>
+                </div>
+            </div>
+        </div>
      );
 }
  
