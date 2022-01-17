c7aa30bb79a716289fc06c5132a1e0f3194ba012
bvenceslas <bvenceslas@gmail.com>
2022-01-17 10:53:49 +0200

    styled add note page

--- frontend/src/components/CreateNote/CreateNote.jsx
+++ frontend/src/components/CreateNote/CreateNote.jsx
@@ -1,6 +1,44 @@
+import './index.css';
+
 const CreateNote = () => {
-    return ( 
-        <h1>Create Note</h1>
+    return (
+        <div className="create-note">
+            <h1>Create Note</h1>
+
+            <form>
+                <div className="form-container">
+                    <div className="input-wrapper">
+                            <label>Titre</label>                        
+                            <input 
+                                type="text"
+                                required
+                            />
+                    </div>
+                    <div className="input-wrapper">
+                        <label>Description</label>                        
+                        <textarea 
+                        >
+                        </textarea>
+                    </div>
+                    <div className="input-wrapper">
+                        <label>Importance</label>
+                        <select 
+                            >
+                            <option value="Homme">Normal</option>
+                            <option value="Femme">Urgent</option>
+                            <option value="Femme">Necessaire</option>
+                            <option value="Femme">A savoir</option>
+                            <option value="Non Specifié">Non Specifiée</option>
+                        </select>
+                    </div>
+                    <div className="input-wrapper">
+                        <label></label>
+                        <button type="submit">Adding Contact</button>
+                    </div>
+                </div>
+                
+            </form>
+        </div>
     );
 }
  
